575a0e63da802c20b45cc12fcc97e5812e188aa2
Diogo Junqueira <didjunqueira89@gmail.com>
2024-12-19 21:36:33 -0300

    teste natal

---
+++ site/search.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Search results</title>
+<meta charset="utf-8">
+<meta name = "format-detection" content = "telephone=no" />
+<link rel="icon" href="img/favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+<meta name="description" content="Your description">
+<meta name="keywords" content="Your keywords">
+<meta name="author" content="Your name">   
+<meta name = "format-detection" content = "telephone=no" /> 
+<meta name="viewport" content="width=device-width,initial-scale=1.0">
+<!--CSS-->
+<link rel="stylesheet" href="css/bootstrap.css">
+<link rel="stylesheet" href="css/responsive.css">
+<link rel="stylesheet" href="css/style.css">
+<!--JS-->
+<script src="js/jquery.js"></script>
+<script src="js/jquery-migrate-1.1.1.js"></script>
+<script src="search/search.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="js/superfish.js"></script>
+<script src="js/jquery.mobilemenu.js"></script>
+<script src="js/jquery.ui.totop.js"></script>
+<!--[if lt IE 8]>
+    <div style='text-align:center'><a href="http://www.microsoft.com/windows/internet-explorer/default.aspx?ocid=ie6_countdown_bannercode"><img src="http://www.theie6countdown.com/images/upgrade.jpg"border="0"alt=""/></a></div>  
+<![endif]-->
+<!--[if lt IE 9]>
+  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+</head>
+<body>
+<div class="global bg">
+<!--header-->
+<header class="border-chess">
+    <article class="up-box">
+        <div class="container">
+            <p class="tel"><span>phone:</span>+1 800 559 6580</p>
+            <p class="log-in"><a href="#">log in or signup</a><img src="img/up_icon.png" alt=""></p>
+        </div>
+        <em></em>
+    </article>
+    <article class="down-box">
+        <div class="container">
+            <h1 class="brand"><a href="index.html"><img src="img/logo.png" alt=""></a></h1>
+            <div id="menu">
+                <div class="navbar navbar_ clearfix">
+                    <div class="navbar-inner">      
+                        <div class="clearfix">
+                            <div class="nav-collapse nav-collapse_ collapse">
+                                <ul class="nav sf-menu clearfix">
+                                    <li><a href="index.html">main</a></li>  
+                                    <li class="sub-menu"><a href="index-1.html">about<span></span></a>
+                                        <ul class="submenu">
+                                            <li><a href="#">about</a></li>
+                                            <li><a href="#">Services</a></li>
+                                            <li><a href="#">Fares</a></li>
+                                            <li><a href="#">Booking</a></li>
+                                            <li><a href="#">Corporate account</a></li>
+                                            <li><a href="#">Vacancies </a></li>
+                                        </ul>
+                                    </li>    
+                                    <li><a href="index-2.html">services</a></li> 
+                                    <li><a href="index-3.html">blog</a></li>
+                                    <li><a href="index-4.html">contacts</a></li>               
+                                </ul>
+                            </div>                                                                                                                
+                        </div>
+                    </div>  
+                </div>
+            </div>
+        </div>
+    </article>
+    <article class="container">
+        <div class="row">
+            <div class="span12 chess">
+                <strong></strong>
+            </div>
+        </div>
+    </article>
+</header>
+    <!--content-->
+<section class="container padBot">
+    <div class="row">
+          <article class="span12">
+                <h2><em>Search<br><span>results</span></em></h2>
+                <div id="search-results"></div>
+          </article>
+    </div>
+</section>
+</div>
+<!--footer-->
+<footer>
+    <div class="container">
+        <div class="row">
+            <article class="span12">
+                <p class="follow pull-right"><a href="#">facebook</a> &bull; <a href="#">twitter</a></p>
+                <p class="pull-left">Taxi Service &copy; 2013 &bull; <br><a href="index-5.html">Privacy Policy</a></p>
+            </article>
+        </div>
+    </div>  
+    <em></em> 
+</footer>
+<script src="js/bootstrap.js"></script>
+</body>
+</html>
